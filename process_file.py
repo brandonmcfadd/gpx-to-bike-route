c2f080c5bb237792270a174c03ed210fac2f7edc
brandon <brandon@brandonmcfadden.com>
2023-07-04 13:11:43 -0500

    v1
    Clean up and better duplicate waypoint handling

--- process_file.py
+++ process_file.py
@@ -1,18 +1,12 @@
 from bs4 import BeautifulSoup
 import json
 
-# input_file_name = input('What is the name of the file to import?\n')
-# input_short_ride_name = input('What is the name of the ride? (ride-name-format)\n')
-# input_ride_name = input('What is the name of the ride?\n')
-# input_ride_desc1 = input('What is the run description line 1?\n')
-# input_ride_desc2 = input('What is the run description line 2?\n')
-# input_brought_by = input('Brought to you by?\n')
-input_file_name = "2023-06-29_1190208808_From Chicago to South Loop.gpx"
-input_short_ride_name = "short-ride-name"
-input_ride_name = "ride name here"
-input_ride_desc1 = "line 1"
-input_ride_desc2 = "line 2"
-input_brought_by = "@bsmcfadden"
+input_file_name = input('What is the name of the file to import?\n')
+input_short_ride_name = input('What is the name of the ride? (ride-name-format)\n')
+input_ride_name = input('What is the name of the ride?\n')
+input_ride_desc1 = input('What is the run description line 1?\n')
+input_ride_desc2 = input('What is the run description line 2?\n')
+input_brought_by = input('Brought to you by?\n')
 with open(input_file_name, 'r') as f:
     data = f.read()
  
@@ -23,14 +17,16 @@ longitudes = []
 waypoints = []
 
 track_points = soup.find_all('trkpt')
-count = 1
+count = 0
 total_length = len(track_points)
+last_lat_lon = []
 for point in track_points:
+    count += 1
     lat = float(point.get('lat'))
     lon = float(point.get('lon'))
     latitudes.append(lat)
     longitudes.append(lon)
-    if count == 1:
+    if count == 1 and ([lat, lon] != last_lat_lon):
         new_waypoint = {
         "name": "Start Point",
         "subtitle": "Address",
@@ -38,6 +34,7 @@ for point in track_points:
         "icon": "https://cdn.glitch.global/d7999431-1914-42d6-90eb-34009ea65e3e/start-button.png?v=1687983578390",
         "coordinates": [lat, lon]
       }
+        print("added start point")
     elif count == total_length:
         new_waypoint = {
         "name": "Finish Point",
@@ -47,14 +44,14 @@ for point in track_points:
         "coordinates": [lat, lon],
         "markerClass": "rotate-minus-35"
       }
-    elif count % 2 != 0:
+    elif ([lat, lon] != last_lat_lon):
         new_waypoint = {
             "waypointOnly": "true",
             "coordinates": [lat, lon],
             "markerClass": ""
         }
+    last_lat_lon = [lat, lon]
     waypoints.append(new_waypoint)
-    count += 1
 
 latitudes_sorted = sorted(latitudes)
 longitudes_sorted = sorted(longitudes)
@@ -74,6 +71,6 @@ output_json = {
     "stops": waypoints
   }
 }
-
-with open("output.json", "w+") as outfile:
-    json.dump(output_json, outfile)
+print(len(waypoints))
+with open(f"output-{input_file_name}.json", "w+") as outfile:
+    json.dump(output_json, outfile, indent=2)
